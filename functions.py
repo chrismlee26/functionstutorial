134c9c234118f3db6af4cb0c4906889998213d39
CHRIS_LEE <chrismlee87@gmail.com>
2020-09-07 00:44:08 -0700

    first commit

---
+++ functions.py
@@ -0,0 +1,42 @@
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5, 6)
+print(result)
+
+result = area(3, 2)
+print(result)
+
+result = area(4, 1)
+print(result)
+
+def subtract(num1, num2):
+    result2 = num1 - num2
+    return result2
+
+result2 = subtract(5, 4)
+print(result2)
+
+result2 = subtract(3, 1)
+print(result2)
+
+result2 = subtract(7, 4)
+print(result2)
+
+def divide(num1, num2):
+    result3 = num1 / num2
+    return result3
+
+    result3 = divide(10, 2)
+    print(result3)
+
+def add(num1, num2):
+    result4 = num1 + num2
+    return result4
+
+result4 = add(7, 4)
+print(result4)
+
+result4 = add(9, 3)
+print(result4)
